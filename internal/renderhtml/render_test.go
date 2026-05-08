02fe2edd99d47ac28786a136d7a44f49bc2c96ba
John Hobbs <john@velvetcache.org>
2026-05-07 22:51:40 -0500

    Add link to footer

--- internal/renderhtml/render_test.go
+++ internal/renderhtml/render_test.go
@@ -230,6 +230,8 @@ func TestRenderGeneratesStaticSite(t *testing.T) {
 	assert.NotContains(t, indexContent, "Foxtrot")
 	assert.NotContains(t, indexContent, "Event details")
 	assert.NotContains(t, indexContent, "entity-secondary")
+	assert.Contains(t, indexContent, "Built by DayZ Event Summary")
+	assert.Contains(t, indexContent, "https://github.com/jmhobbs/DayZ-Event-Summary")
 	assert.True(t, strings.Index(indexContent, "href=\"teams/index.html\">View All") < strings.Index(indexContent, "href=\"players/index.html\">View All"))
 
 	assert.Contains(t, playersIndexContent, "Full player leaderboard.")
